21cae3e35c273b7770d75ab24819087c71887085
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-23 11:45:44 -0700

    Added binary-search code

---
+++ Kunal-DSA/src/com/binarySearch/BinarySearch.java
@@ -0,0 +1,35 @@
+package com.binarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        int[] arr = {-2, -1, 3, 6, 7, 9, 22, 45, 68};
+        int target = 689;
+
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+
+    }
+
+    // return the index
+    // return -1 if it doesn't exist
+    static int binarySearch(int[] arr, int target){
+
+        int start = 0;
+        int end = arr.length;
+
+        while(start <= end){
+
+            int mid = start + (end - start)/2;
+            if (target == arr[mid]){
+                return mid;
+            } else if (target < arr[mid]){
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}

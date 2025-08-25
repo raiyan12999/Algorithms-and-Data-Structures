22c8d627446ee332956ff79d46b57af455f0a207
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-25 08:28:41 -0700

    Added the solution of leetcode 34

---
+++ Kunal-DSA/src/com/binarySearch/FirstAndLast.java
@@ -0,0 +1,54 @@
+package com.binarySearch;
+
+public class FirstAndLast {
+
+    public static void main(String[] args) {
+       int [] nums = {5, 7, 7, 7, 7, 8, 8, 10};
+       int target = 7;
+
+       int[] ans = searchRange(nums, target);
+       System.out.println(ans.toString());
+    }
+
+    static int[] searchRange(int[] nums, int target){
+
+        int[] ans = {-1, -1};
+
+        // check for first occurence of target
+        ans[0] = search(nums, target, true);
+        if(ans[0] != -1){
+            ans[1] = search(nums, target, false);
+        }
+
+        return ans;
+    }
+
+    // this function just returns the index value of target
+    static int search(int[] nums, int target, boolean findStartIndex){
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end){
+            // find the middle element
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]){
+                end = mid - 1;
+            } else if (target > nums[mid]){
+                start = mid + 1;
+            } else {
+                // potential ans found
+                ans = mid;
+
+                if(findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+}

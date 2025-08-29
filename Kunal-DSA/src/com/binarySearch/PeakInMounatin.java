b772facb7870e0016ba3abe045ab7c8278c82116
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-08-29 04:53:44 -0700

    solution with o(logn) complexity

--- Kunal-DSA/src/com/binarySearch/PeakInMounatin.java
+++ Kunal-DSA/src/com/binarySearch/PeakInMounatin.java
@@ -3,7 +3,7 @@ package com.binarySearch;
 public class PeakInMounatin {
 
     public static void main(String[] args) {
-        int[] arr = {0, 3, 4, 6, 5, 2, 1};
+        int[] arr = {3,5,3,2,0};
         int ans = peakIndexInMountainArray(arr);
 
         System.out.println(ans + " " + arr[ans]);
@@ -11,17 +11,34 @@ public class PeakInMounatin {
 
     static int peakIndexInMountainArray(int[] arr) {
 
-        int max = arr[0];
-        int index = 0;
+        int start = 0;
+        int end = arr.length - 1;
 
-        for (int i = 1; i <= arr.length - 1; i++){
-            if (arr[i] > max){
-                max = arr[i];
-                index = i;
+        while (start <= end){
+
+            int mid = start + (end - start)/2;
+
+            if (mid == arr.length - 1){
+                end = mid - 1;
+                mid = mid - 1;
+            } else if (mid == 0){
+                start = mid + 1;
+                mid = mid + 1;
+            }
+
+            if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]){
+
+                start = mid + 1;
+            } else if (arr[mid] < arr[mid - 1] && arr[mid] > arr[mid + 1]){
+
+                end = mid - 1;
+            } else {
+
+                return mid;
             }
         }
 
-        return index;
+        return -1;
 
 
     }

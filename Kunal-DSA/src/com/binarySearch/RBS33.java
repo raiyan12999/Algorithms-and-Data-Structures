7accb73a00f7a239a688795a2dd732cdd5da856e
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-09-01 13:17:06 -0700

    failing in edge case, working for some

--- Kunal-DSA/src/com/binarySearch/RBS33.java
+++ Kunal-DSA/src/com/binarySearch/RBS33.java
@@ -3,7 +3,7 @@ package com.binarySearch;
 public class RBS33 {
 
     public static void main(String[] args) {
-        int[] nums = {4,5,6,7,0,1,2};
+        int[] nums = {1,3};
         int target = 0;
         int ans = search(nums, target);
         System.out.println(ans);
@@ -21,11 +21,22 @@ public class RBS33 {
             if (nums[mid] > nums[mid + 1]){
                 pivot = mid;
 
-                return pivot;
-            } else if (nums[mid] < nums[mid - 1]){
+                break;
+            } else if (nums[mid] != nums[0] && nums[mid] <= nums[mid - 1]) {
                 pivot = mid - 1;
 
-                return pivot;
+                break;
+            } else if(nums[mid] == nums[0]){
+                if (nums[mid] > nums[mid + 1]){
+                    pivot = mid;
+
+                    break;
+                } else if (nums[mid] < nums[mid + 1]){
+                    pivot = mid + 1;
+
+                    break;
+                }
+
             } else if (nums[start] > nums[mid]){
                 end = mid - 1;
             } else if (nums[start] < nums[mid]){
@@ -33,6 +44,39 @@ public class RBS33 {
             }
         }
 
-        return pivot;
+        start = 0;
+        end = pivot;
+
+        int ans1 = binarySearch(nums, target, start, end);
+
+        start = pivot + 1;
+        end = nums.length - 1;
+        int ans2 = binarySearch(nums, target, start, end);
+
+        if(ans1 != -1){
+            return ans1;
+        } else if(ans2 != -1){
+            return ans2;
+        }
+
+        return ans1;
+    }
+
+    static int binarySearch(int[] nums, int target, int start, int end){
+
+        while (start <= end){
+
+            int mid = start + (end - start)/2;
+
+            if (nums[mid] == target){
+                return mid;
+            } else if (nums[mid] < target){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }

58a23f0e855bfd550918c172d6257511d7f98079
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-09-04 02:46:52 -0700

    cleaned one

--- Kunal-DSA/src/com/sorting/vjudgeBubbleSort.java
+++ Kunal-DSA/src/com/sorting/vjudgeBubbleSort.java
@@ -16,6 +16,42 @@ public class vjudgeBubbleSort {
             arr[i] = sc.nextInt();
         }
 
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
+        printArray(arr);
+        bubble(arr);
+        printArray(arr);
+//        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+
+    static void bubble(int[] nums){
+
+        boolean swapped;
+
+        for (int i = 0; i < nums.length - 1; i++){
+            swapped = false;
+            for (int j = 0; j < nums.length - 1 - i; j++){
+                if (nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                    swapped = true;
+                }
+//                System.out.println(Arrays.toString(nums));
+            }
+
+            if(!swapped){
+                break;
+            }
+        }
+    }
+
+    static void printArray(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            System.out.print(nums[i]);
+            if(i < nums.length){
+                System.out.print(" ");
+            }
+        }
+//        System.out.println();
     }
 }

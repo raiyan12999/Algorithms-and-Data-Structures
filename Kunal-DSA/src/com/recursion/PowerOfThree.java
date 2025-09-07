9b9d81b1aca80ebe08fae113a768c3f5587251bf
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-09-07 12:19:26 -0700

    Recursive solution

--- Kunal-DSA/src/com/recursion/PowerOfThree.java
+++ Kunal-DSA/src/com/recursion/PowerOfThree.java
@@ -4,19 +4,38 @@ public class PowerOfThree {
 
     public static void main(String[] args) {
 
-        System.out.println(isPowerOfThree(81));
+        System.out.println(isPowerOfThree(80));
 //        System.out.println(4/3);
     }
 
     static boolean isPowerOfThree(int n){
 
-        for (int i = 3; i <= n; i = i * 3){
-            if(i == n){
-                return true;
-            }
+//        for (int i = 3; i <= n; i = i * 3){
+//            if(i == n){
+//                return true;
+//            }
+//
+//        }
+//
+//        return false;
 
+        long number = (long) n;
+        long x = 1;
+        boolean ans = compare(x, number);
+
+        return ans;
+
+    }
+
+    static boolean compare(long x, long number){
+        if ( x == number){
+            return true;
+        } else if (x > number){
+            return false;
         }
 
-        return false;
+        return compare(x * 3, number);
     }
+
+
 }
